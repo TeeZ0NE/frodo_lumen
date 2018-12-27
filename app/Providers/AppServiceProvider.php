6a1444fc1a773d7270b70dfea3a9dd2368acc0d6
Vadim Kov <vadim-kovalchuk@riseup.net>
2018-12-27 14:35:21 +0200

    Add ServiceProvider with switched off data wrapping in JSON 2 data array

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,21 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Resources\Json\Resource;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
+	/**
+	 * Perform post-registration booting of services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		Resource::withoutWrapping();
+	}
+
+	/**
      * Register any application services.
      *
      * @return void
